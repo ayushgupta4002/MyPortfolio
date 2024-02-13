d192bfa09caee4f50ed6953629ed2c86f3ec54d3
AYUSH GUPTA <66496371+ayushgupta4002@users.noreply.github.com>
2024-02-14 00:40:10 +0530

    flex added in skills

--- src/Components/Mainbody.jsx
+++ src/Components/Mainbody.jsx
@@ -99,7 +99,11 @@ function Mainbody() {
           </div>
           
           <div className="pt-[1vh] font">
-            <b>Backend Development : </b>Node.js,Typescript,GraphQl,Express.js,Cargo,GoLang,Git,Github
+            <div class="flex flex-row">
+              <div> <b>Backend Development : </b></div>
+              <div>Node.js,Typescript,GraphQl,Express.js,Cargo,GoLang,Git,Github</div>
+            </div>
+           
           </div>
           <div className="pt-[1vh] font">
             <b>DevOps and Cloud : </b>CI/CD,Jenkins,Kubernetes,Docker,AWS,GCP
